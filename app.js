fd4c531686dc76a6946ca99035e957a457cc647a
Duyvo1117 <duyvo1117@gmail.com>
2022-08-21 18:23:44 -0400

    implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,15 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // GLOBAL MIDDLEWARE
+// Implement CORS
+app.use(cors());
+// set Access-Control-Allow-Origin *
+// app.use(cors({ origin: 'https://www.myapp.com' }));
+
+// For non simple requests
+app.options('*', cors());
+// app.options('api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
