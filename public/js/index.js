f78f17a98a37378c97d3fe50ca843236d5e348e5
Duyvo1117 <duyvo1117@gmail.com>
2022-08-21 23:20:24 -0400

    fixed bugs

--- public/js/index.js
+++ public/js/index.js
@@ -95,7 +95,7 @@ if (bookBtn) {
 
 const alertMessage = document.querySelector('body').dataset.alert;
 if (alertMessage) {
-  showAlert('success', alertMessage, 20);
+  showAlert('success', alertMessage, 15);
 }
 
 // "passwordCurrent":"pass1234",
